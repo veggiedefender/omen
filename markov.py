ed1451abafe0b09f0775c6d1fa3f59078aaab497
veggiedefender <jessejesse123@gmail.com>
2018-02-02 21:55:02 -0500

    store running counts instead of duplicate words

--- markov.py
+++ markov.py
@@ -1,10 +1,18 @@
-import nltk
-from nltk.tokenize.moses import MosesDetokenizer
 import random
+import nltk
+from collections import defaultdict
+from nltk.tokenize.moses import MosesDetokenizer
 
 START = "@@_START_@@"
 END = "@@_END_@@"
 STATE_SIZE = 3
+
+def weighted_choice(words):
+    number = random.random() * sum(words.values())
+    for key, weight in words.items():
+        number -= weight
+        if number < 0:
+            return key
 
 with open("corpus.txt") as f:
     corpus = f.read()
@@ -12,22 +20,19 @@ with open("corpus.txt") as f:
 sentences = nltk.sent_tokenize(corpus)
 sentences = [[START, *nltk.word_tokenize(sentence), END] for sentence in sentences]
 
-states = {}
+states = defaultdict(lambda: defaultdict(int))
 beginnings = []
 for sentence in sentences:
     beginnings.append(tuple(sentence[:STATE_SIZE]))
     for index, word in enumerate(sentence):
         key = tuple(sentence[index - STATE_SIZE:index])
-        try:
-            states[key].append(word)
-        except KeyError:
-            states[key] = [word]
+        states[key][word] += 1
 
 tokens = list(random.choice(beginnings))
 curr = tokens[-1]
 while curr != END:
     key = tuple(tokens[-STATE_SIZE:])
-    curr = random.choice(states[key])
+    curr = weighted_choice(states[key])
     tokens.append(curr)
 
 detokenizer = MosesDetokenizer()
